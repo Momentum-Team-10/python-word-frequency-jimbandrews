12bb1d12beb45271b640cdcc0ad0b664ff8e1257
James Andrews <jbandrew14@gmail.com>
2021-10-26 14:36:47 -0400

    copied function text

--- test.py
+++ test.py
@@ -1,12 +1,41 @@
 import string
-# file = "praise_song_for_the_day.txt"
-text = "Here’s what you’ll learn in this tutorial:"
-zero_punctuation = text
-for char in string.punctuation:
-    if char in text:
-        zero_punctuation = zero_punctuation.replace(char, "")
 
-print(zero_punctuation)
+STOP_WORDS = [
+    'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
+    'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
+    'will', 'with'
+]
+
+
+def print_word_freq(file):
+    """Read in `file` and print out the frequency of words in that file."""
+    # reading file string and setting it to variable
+    with open(file) as opened_file:
+        file_string = opened_file.read()
+    
+    # make all letters lowercase
+    lowercase = file_string.lower()
+
+    # delete all punctuation
+    punctuation = string.punctuation + "’"
+    zero_punctuation = lowercase
+    for char in punctuation:
+        if char in zero_punctuation:
+            zero_punctuation = zero_punctuation.replace(char, "")
+    
+    word_dict = {}
+    word_list = zero_punctuation.split(" ")
+    for word in word_list:
+        if word in STOP_WORDS:
+            continue
+        elif word in word_dict.keys():
+            word_dict[word] += 1
+        else:
+            word_dict[word] = 1
+        
+    return print(word_dict)
+
+print_word_freq()
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # import string
