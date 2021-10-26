7d79b0241c044a40d6f7d30cd91edfd376a7e2a9
James Andrews <jbandrew14@gmail.com>
2021-10-26 16:32:31 -0400

    added comments, changed print statement to an f-string

--- word_frequency.py
+++ word_frequency.py
@@ -12,47 +12,60 @@ def print_word_freq(file):
     # reading file string and setting it to variable
     with open(file) as opened_file:
         file_string = opened_file.read()
-    
+
     # make all letters lowercase
     lowercase = file_string.lower()
 
     # delete all punctuation
     punctuation = string.punctuation + "’"
-    replace_w_space = ["\n", "—", "  "]
     zero_punctuation = lowercase
     for char in punctuation:
         if char in zero_punctuation:
             zero_punctuation = zero_punctuation.replace(char, "")
+
+    # replace certain characters with a single white space
+    replace_w_space = ["\n", "—", "  "]
     for char in replace_w_space:
         if char in zero_punctuation:
             zero_punctuation = zero_punctuation.replace(char, " ")
     
-    # create and fill word_count dictionary
+    # initialize variables for later, and split my string into a list
     word_count = {}
-    word_list = zero_punctuation.split(" ")
     max_length = 1
+    word_list = zero_punctuation.split(" ")
+
     for word in word_list:
+        # flow control for string we don't need to count
         if word in STOP_WORDS:
             continue
         elif word == "":
             continue
+
+        # check if word is in dict from above, then either add the word
+        # or 1 to the key's value
         elif word in word_count.keys():
             word_count[word] += 1
         else:
             word_count[word] = 1
         
+        # used to find the length of the longest word for later use
         if len(word) > max_length:
             max_length = len(word)
+    
+    # create a list of keys sorted in descending order based on their key value
     words_by_freq = sorted(word_count, key=word_count.get, reverse=True)
 
     for word in words_by_freq:
         frequency = word_count[word]
+        # determine number of spaces needed to align the | character
         spaces = (max_length - len(word)) * " "
+        # determine number of stars and number of spaces to align stars
         if frequency < 10:
             stars = "  " + frequency * "*"
         else:
             stars = " " + frequency * "*"
-        print(spaces + word + " | " + str(frequency) + stars)
+        # finish by printing an f-string for each word
+        print(f" {spaces}{word} | {frequency}{stars}")
 
 
 if __name__ == "__main__":
